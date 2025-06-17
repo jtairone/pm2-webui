db339925e20b014a3cebdce92ada16f77e758425
Tairone Morais <jtaironemorais@hotmail.com>
2025-06-16 23:58:01 -0300

    Implementado o sweetAlert nos botão de ações, proximo passo e implementar no form add app

--- src/public/assets/js/main.js
+++ src/public/assets/js/main.js
@@ -1,4 +1,38 @@
 async function pm2AppAction(appName, action){
-    await fetch(`/api/apps/${appName}/${action}`, { method: 'POST'})
-    location.reload();
+    if(action == 'delete'){
+        swal.fire({
+            title: 'Tem certeza disso?',
+            icon: 'question',
+            showCancelButton: true,
+            confirmButtonText: 'SIM',
+            cancelButtonText: 'NÂO',
+            cancelButtonColor: 'Red',
+            confirmButtonColor: '#2AA13D',
+            reverseButtons: true
+        }).then( async (res)=>{
+            if(res.isConfirmed){
+                await fetch(`/api/apps/${appName}/${action}`, { method: 'POST'})
+                location.reload();
+            }
+        })
+    }else{
+        const Toast = Swal.mixin({
+            toast: true,
+            position: "top-end",
+            showConfirmButton: false,
+            timer: 3000,
+            timerProgressBar: true,
+            didOpen: (toast) => {
+                toast.onmouseenter = Swal.stopTimer;
+                toast.onmouseleave = Swal.resumeTimer;
+            }
+        });
+        Toast.fire({
+            icon: "success",
+            title:  `${action} realizado sucesso!`
+        }).then( async ()=>{
+                await fetch(`/api/apps/${appName}/${action}`, { method: 'POST'})
+                location.reload();
+        });
+    }
 }
